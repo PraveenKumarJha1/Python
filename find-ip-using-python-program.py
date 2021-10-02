fe2795fe4d0ecb53fd7fc0ce13844897cb41937f
Praveen Kumar Jha <32644784+PraveenKumarJha1@users.noreply.github.com>
2021-10-02 12:13:19 +0530

    version2

--- find-ip-using-python-program.py
+++ find-ip-using-python-program.py
@@ -16,7 +16,7 @@ for ifaceName in interfaces():
 	addresses = [i['addr']
 	for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
 	print(' '.join(addresses))
-'''
+''' 
 #--------------------------------------------------------------------------------------------------------
   #third Methord
 ## importing socket module
